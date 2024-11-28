3e6ea0c183d8a928c9b03cec87fa516d9de8d788
gujianjie <gujianjie@able-elec.com>
2024-11-28 14:47:48 +0800

    feat: 业务场景表定义

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,18 +1,20 @@
 import * as t from 'drizzle-orm/mysql-core'
 
-export const house = t.mysqlTable('house', {
-  id: t.int('id').autoincrement().primaryKey(),
-  author_id: t.int('author_id').notNull().$default(() => 0),
-  title: t.varchar('title').notNull(),
-  content: t.text('content').notNull(),
-  tags: t.text('tags').notNull(),
-  like_count: t.int('like_count').notNull().$default(() => 0),
-  collection_count: t.int('collection_count').notNull().$default(() => 0),
-  house_info_id: t.int('house_info_id'),
-  conver: t.text('cover').notNull(),
-  swiper: t.text('swiper').notNull()
+export const houseInfo = t.mysqlTable('house_info', {
+  id: t.int().autoincrement().notNull(),
+  code: t.varchar({ length: 256 }).notNull(),
+  address: t.varchar({ length: 256 }).notNull()
 })
 
-export const houseInfo = t.mysqlTable('house_info', {
-  
+export const house = t.mysqlTable('house', {
+  id: t.int().autoincrement().primaryKey(),
+  author_id: t.int().notNull().$default(() => 0),
+  title: t.varchar({ length: 256 }).notNull(),
+  content: t.text().notNull(),
+  tags: t.text().notNull(),
+  like_count: t.int().notNull().$default(() => 0),
+  collection_count: t.int().notNull().$default(() => 0),
+  house_info_id: t.int().references(() => houseInfo.id),
+  conver: t.text().notNull(),
+  swiper: t.text().notNull()
 })
