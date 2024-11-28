368cb982b1eaf3b56ad7cfe7649469fa3cf62cc1
gujianjie <gujianjie@able-elec.com>
2024-11-28 14:16:45 +0800

    feat: 表定义

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,18 @@
-import { int, mysqlTable, serial, varchar } from 'drizzle-orm/mysql-core'
+import * as t from 'drizzle-orm/mysql-core'
 
-export const userTable = mysqlTable('users_table', {
-  id: serial().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  age: int().notNull(),
-  email: varchar({ length: 255 }).notNull().unique()
+export const house = t.mysqlTable('house', {
+  id: t.int('id').autoincrement().primaryKey(),
+  author_id: t.int('author_id').notNull().$default(() => 0),
+  title: t.varchar('title').notNull(),
+  content: t.text('content').notNull(),
+  tags: t.text('tags').notNull(),
+  like_count: t.int('like_count').notNull().$default(() => 0),
+  collection_count: t.int('collection_count').notNull().$default(() => 0),
+  house_info_id: t.int('house_info_id'),
+  conver: t.text('cover').notNull(),
+  swiper: t.text('swiper').notNull()
+})
+
+export const houseInfo = t.mysqlTable('house_info', {
+  
 })
