c02570bc7272253c2faf0c5d874f5df1e1f68623
gujianjie <gujianjie@able-elec.com>
2024-11-28 11:03:37 +0800

    feat: 查询接口

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { createFactory } from 'hono/factory'
+import { eq } from 'drizzle-orm'
 import { db } from './db'
+import { userTable } from './db/schema'
 
 const app = new Hono()
 const factory = createFactory()
 
-const handlers = factory.createHandlers(c => {
-  return c.json({ name: 'gujianjie' })
+const handlers = factory.createHandlers(async c => {
+  const users = await db.select().from(userTable)
+
+  return c.json({ msg: 'ok', data: users })
 })
 
 app.get('/api/schema', ...handlers)
+
+app.onError((err, c) => {
+  console.error(`${err}`)
+
+  return c.text('this is error', 500)
+})
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
