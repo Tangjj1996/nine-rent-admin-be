2e56af03d305e0a5e74317b7e388da107a5816da
gujianjie <gujianjie@able-elec.com>
2024-11-28 22:26:28 +0800

    feat: 类型补充

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { createFactory } from 'hono/factory'
-import { eq } from 'drizzle-orm'
-import { z } from 'zod'
-import { db } from './db'
-import { house } from './db/schema'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { createFactory } from "hono/factory";
+import { eq } from "drizzle-orm";
+import { z } from "zod";
+import { omit } from "lodash-es";
+import { db } from "./db";
+import { house } from "./db/schema";
 
-const app = new Hono()
-const factory = createFactory()
+const app = new Hono();
+const factory = createFactory();
 
-const getSchemaHandle = factory.createHandlers(async c => {
-  const users = await db.select().from(house)
+const getSchemaHandle = factory.createHandlers(async (c) => {
+  const users = await db.select().from(house);
 
-  return c.json({ msg: 'ok', data: users })
-})
+  return c.json({ msg: "ok", data: users });
+});
 
-app.get('/api/schema', ...getSchemaHandle)
-
-async function updateOrInsertUserData(userData: any) {
-  const { id } = userData
-
-  const existingHouse = await db
-    .select()
-    .from(house)
-    .where(eq(house.id, id))
-    .execute()
-
-  if (existingHouse.length > 0) {
-    // 存在则更新
-    await db
-      .update(house)
-      .set(userData)
-      .where(eq(house.id, id))
-      .execute()
-  } else {
-    // 不存在则新建
-    await db
-      .insert(house)
-      .values(userData)
-      .execute()
-  }
-}
+app.get("/api/schema", ...getSchemaHandle);
 
 const userDataSchema = z.object({
   id: z.number(),
@@ -52,31 +28,60 @@ const userDataSchema = z.object({
   collection_count: z.number(),
   house_info_id: z.number(),
   cover: z.string().url(),
-  swiper: z.array(z.string().url())
-})
+  swiper: z.array(z.string().url()),
+});
 
-const modifySchemaHandle = factory.createHandlers(async c => {
-  const userData = await c.req.json()
-  const { success, error } = userDataSchema.safeParse(userData)
+async function updateOrInsertUserData(userData: any) {
+  const { id } = userData;
+
+  const existingHouse = await db
+    .select()
+    .from(house)
+    .where(eq(house.id, id))
+    .execute();
+
+  if (existingHouse.length > 0) {
+    // 存在则更新
+    await db.update(house).set(userData).where(eq(house.id, id)).execute();
+  } else {
+    // 不存在则新建
+    await db.insert(house).values(userData).execute();
+  }
+}
+
+const modifySchemaHandle = factory.createHandlers(async (c) => {
+  const userData = await c.req.json();
+  const { success, error, data } = userDataSchema.safeParse(userData);
   if (success) {
-    return c.json({ msg: 'ok', data: null })
+    const composeData: Omit<typeof data, "tags" | "swiper"> & {
+      tags: string;
+      swiper: string;
+    } = {
+      tags: "",
+      swiper: "",
+      ...omit(data, ["tags", "swiper"]),
+    };
+    composeData.tags = data.tags.join(",");
+    composeData.swiper = data.swiper.join(",");
+    await updateOrInsertUserData(composeData);
+    return c.json({ msg: "ok", data: null });
   }
 
-  return c.json({ msg: error.message, data: null, code: 400 })
-})
+  return c.json({ msg: error.message, data: null, code: 400 });
+});
 
-app.post('/api/schema_update', ...modifySchemaHandle)
+app.post("/api/schema_update", ...modifySchemaHandle);
 
 app.onError((err, c) => {
-  console.error(`${err}`)
+  console.error(`${err}`);
 
-  return c.json({ msg: err.message, data: null, code: 400 })
-})
+  return c.json({ msg: err.message, data: null, code: 400 });
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
